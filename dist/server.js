52c102d3ee75bd5c732b39780bc55859d6c0d5e9
Ariful Islam <remalarif0130@gmail.com>
2024-05-22 21:54:15 +0600

    eslint added to fix code error

--- dist/server.js
+++ dist/server.js
@@ -17,10 +17,15 @@ const config_1 = __importDefault(require("./app/config"));
 const mongoose_1 = __importDefault(require("mongoose"));
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
-        yield mongoose_1.default.connect(config_1.default.database_url);
-        app_1.default.listen(config_1.default.port, () => {
-            console.log(`Example app listening on port ${config_1.default.port}`);
-        });
+        try {
+            yield mongoose_1.default.connect(config_1.default.database_url);
+            app_1.default.listen(config_1.default.port, () => {
+                console.log(`Example app listening on port ${config_1.default.port}`);
+            });
+        }
+        catch (err) {
+            console.log(err);
+        }
         // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
     });
 }
